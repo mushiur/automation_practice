a1ba9b74fb2b32b9fc9b6cd79940a593c243f614
excited <rifat1520154@gmail.com>
2022-10-17 11:21:20 +0600

    checkout addded

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.service import Service
 
 first_name = "Rifat"
 last_name = "Rahman"
-email = "rifat152015@gmail.com"
+email = "rit125071@mail.com"
 password = "Robi@12345"
 company = "Red.dot"
 address = "12/2,Gulshan,Red.dot"
@@ -21,7 +21,8 @@ driver = webdriver.Chrome(service=Service('C:\Driver\chromedriver_win32\chromedr
 driver.get("http://automationpractice.com/index.php")
 print(driver.title)
 
-driver.find_element("xpath", "//*[@id='header']/div[2]/div/div/nav/div[1]/a").click()
+driver.maximize_window()
+driver.find_element("xpath", "//a[@class='login']").click()
 time.sleep(1)
 
 element = driver.find_element("id", "email_create")
@@ -99,6 +100,34 @@ element = driver.find_element("id", "alias")
 element.send_keys(address)
 
 driver.find_element("xpath", "//button[@id='submitAccount']").click()
+
+# search shirt
+element = driver.find_element("id", "search_query_top")
+element.send_keys("shirt")
+
+# search button
+driver.find_element("xpath", "//button[@name='submit_search']").click()
+time.sleep(2)
+
+# add to cart
+time.sleep(10)
+driver.find_element("xpath", "//span[normalize-space()='Add to cart']").click()
+time.sleep(10)
+
+# proceed to checkout
+driver.find_element("xpath", "//span[normalize-space()='Proceed to checkout']").click()
+time.sleep(5)
+
+driver.find_element("xpath", "//*[@id='center_column']/p[2]/a[1]/span").click()
+time.sleep(2)
+
+driver.find_element("xpath", "//*[@id='center_column']/form/p/button/span").click()
+time.sleep(2)
+
+driver.find_element("xpath", "//input[@id='cgv']").click()
+time.sleep(2)
+driver.find_element("xpath", "//*[@id='form']/p/button/span").click()
+
 
 
 # driver.close()  # current focus browser
